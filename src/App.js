c4e88e6e1aa2956cf4060bdefb8b5c2587792bda
Vanesa G <87158362+vanesa5@users.noreply.github.com>
2022-02-04 16:32:39 -0500

    imported filterData array.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import FiltersList from "./Filters/index";
 //TODO: import the data model for your list of filters
-
+import {filterData} from "./data.js"
 const App = () => (
   <div className="App">
     {/* TODO: pass your data model as a prop to your FiltersList component */}
